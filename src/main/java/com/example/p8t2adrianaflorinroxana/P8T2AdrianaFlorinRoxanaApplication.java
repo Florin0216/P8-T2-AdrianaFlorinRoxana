354ce2fcec949ce2e17f8c21ce29c2ad54bac31a
Florin0216 <florin.barbu16@yahoo.com>
2025-03-21 14:36:28 +0200

    Initial commit

---
+++ src/main/java/com/example/p8t2adrianaflorinroxana/P8T2AdrianaFlorinRoxanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.p8t2adrianaflorinroxana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P8T2AdrianaFlorinRoxanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P8T2AdrianaFlorinRoxanaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/p8t2adrianaflorinroxana/P8T2AdrianaFlorinRoxanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p8t2adrianaflorinroxana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P8T2AdrianaFlorinRoxanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
